52393039a3b9c41247b9500ce63d30992884dab2
surya-deepak-pudi <48729818+surya-deepak-pudi@users.noreply.github.com>
2021-12-21 19:05:52 +0530

    initialized the app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { Typography } from "@material-ui/core"
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Typography variant="h4" component={"h1"}>
+        Cryptracker
+      </Typography>
+    </>
+  )
 }
 
-export default App;
+export default App
